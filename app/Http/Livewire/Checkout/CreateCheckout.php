2841e000ab79abca3a4db72417ce59b54c616712
IksanFN <iksanfauzi727@gmail.com>
2023-01-09 11:21:39 +0700

    Install & Configuration Livewire

---
+++ app/Http/Livewire/Checkout/CreateCheckout.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Checkout;
+
+use Livewire\Component;
+
+class CreateCheckout extends Component
+{
+    public function render()
+    {
+        return view('checkout.create');
+    }
+}
